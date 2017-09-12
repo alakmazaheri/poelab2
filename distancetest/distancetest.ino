1653ae57430c9362068372f1f2dfa4e48ad14319
alakmazaheri <avalakmazaheri@gmail.com>
2017-09-12 11:09:54 -0400

    upload day 1 files

---
+++ distancetest/distancetest.ino
@@ -0,0 +1,11 @@
+const byte analogInPin = A0;
+float sensor_value;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensor_value = analogRead(analogInPin);
+  Serial.println(sensor_value);
+}
